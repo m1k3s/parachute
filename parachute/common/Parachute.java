85a3adda60c066bd85411e10c3763c3c71af49bd
m1k3s <pbrane0@gmail.com>
2015-12-30 08:03:00 -0500

    updated to forge-1.8.9-11.15.0.1659

--- parachute/common/Parachute.java
+++ parachute/common/Parachute.java
@@ -41,8 +41,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class Parachute {
 
 	public static final String modid = "parachutemod";
-	public static final String modversion = "1.0.2";
-	public static final String mcversion = "1.8.8";
+	public static final String modversion = "1.0.3";
+	public static final String mcversion = "1.8.9";
 	public static final String name = "Parachute Mod NG";
 	public static final String guifactory = "com.parachute.client.ParachuteConfigGUIFactory";
 	public static StatBasic parachuteDeployed = new StatBasic("stat.parachuteDeployed", new ChatComponentTranslation("stat.parachuteDeployed"));
