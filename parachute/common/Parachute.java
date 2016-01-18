0a3f0f29066e063eb099145f707604b1f5f4524a
m1k3s <pbrane0@gmail.com>
2016-01-18 12:55:37 -0500

    added all strings to language files. These need new translations. bumped minor version

--- parachute/common/Parachute.java
+++ parachute/common/Parachute.java
@@ -42,7 +42,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class Parachute {
 
 	public static final String modid = "parachutemod";
-	public static final String modversion = "1.2.1";
+	public static final String modversion = "1.2.2";
 	public static final String mcversion = "1.8.9";
 	public static final String name = "Parachute Mod NG";
 	public static final String guifactory = "com.parachute.client.ParachuteConfigGUIFactory";
