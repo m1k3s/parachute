aa3eaa015520d94969b196eac64405a07f0548be
m1k3s <pbrane0@gmail.com>
2016-06-23 06:50:46 -0400

    updated version number to MC 1.10 and mod 1.5.0

--- parachute/common/Parachute.java
+++ parachute/common/Parachute.java
@@ -43,8 +43,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class Parachute {
 
 	public static final String modid = "parachutemod";
-	public static final String modversion = "1.4.3";
-	public static final String mcversion = "1.9.4";
+	public static final String modversion = "1.5.0";
+	public static final String mcversion = "1.1o";
 	public static final String name = "Parachute Mod NG";
 	public static final String guifactory = "com.parachute.client.ParachuteConfigGUIFactory";
 	public static StatBasic parachuteDeployed = new StatBasic("stat.parachuteDeployed", new TextComponentTranslation("stat.parachuteDeployed"));
