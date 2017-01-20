557fc057b12724e821606babbe27154c969f5d7f
m1k3s <pbrane0@gmail.com>
2017-01-19 20:10:15 -0500

    changed livinghurtevent test

--- parachute/common/PlayerHurtEvent.java
+++ parachute/common/PlayerHurtEvent.java
@@ -1,6 +1,5 @@
 package com.parachute.common;
 
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -8,7 +7,7 @@ public class PlayerHurtEvent {
     @SuppressWarnings("unused")
     @SubscribeEvent
     public void SkydiverHurtEvent(LivingHurtEvent event) {
-        if (event.getEntityLiving() instanceof EntityPlayer && ParachuteCommonProxy.isDeployed()) {
+        if (event.getEntityLiving().isRiding() && ParachuteCommonProxy.isDeployed()) {
             event.setCanceled(true);
             event.setAmount(0.0f);
         }
