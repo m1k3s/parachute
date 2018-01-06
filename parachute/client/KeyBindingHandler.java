fad0c8d3aa0eb6cc4a3d8f91962e108238bf2fe6
m1k3s <pbrane0@gmail.com>
2018-01-06 07:53:55 -0500

    removed powered flight, KeyBindingHandler commented out.

--- parachute/client/KeyBindingHandler.java
+++ parachute/client/KeyBindingHandler.java
@@ -22,22 +22,22 @@
 
 package com.parachute.client;
 
-import com.parachute.common.ConfigHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
-
-@SuppressWarnings("unused")
-
-@Mod.EventBusSubscriber(Side.CLIENT)
-public class KeyBindingHandler {
-    @SubscribeEvent
-    public static void clientTick(final TickEvent.ClientTickEvent event) {
-        if (event.phase != TickEvent.Phase.END) return;
-
-        if (ModKeyBindings.POWEREDFLIGHT.isPressed()) {
-            ConfigHandler.togglePoweredFlight();
-        }
-    }
-}
+//import com.parachute.common.ConfigHandler;
+//import net.minecraftforge.fml.common.Mod;
+//import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+//import net.minecraftforge.fml.common.gameevent.TickEvent;
+//import net.minecraftforge.fml.relauncher.Side;
+//
+//@SuppressWarnings("unused")
+//
+//@Mod.EventBusSubscriber(Side.CLIENT)
+//public class KeyBindingHandler {
+//    @SubscribeEvent
+//    public static void clientTick(final TickEvent.ClientTickEvent event) {
+//        if (event.phase != TickEvent.Phase.END) return;
+//
+//        if (ModKeyBindings.POWEREDFLIGHT.isPressed()) {
+//            ConfigHandler.togglePoweredFlight();
+//        }
+//    }
+//}
